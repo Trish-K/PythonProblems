99496785aeb2f130a82c4387c0f59f40b12472fd
Patricia Klemp <97997271+Trish-K@users.noreply.github.com>
2022-01-22 01:56:52 +0000

    updated answers to be more efficient

--- labs109.py
+++ labs109.py
@@ -1,140 +1,67 @@
-# As an example, here is an implementation of
-# the first problem "Ryerson Letter Grade":
-
-# def ryerson_letter_grade(n):
-#     if n < 50:
-#         return 'F'
-#     elif n > 89:
-#         return 'A+'
-#     elif n > 84:
-#         return 'A'
-#     elif n > 79:
-#         return 'A-'
-#     tens = n // 10
-#     ones = n % 10
-#     if ones < 3:
-#         adjust = "-"
-#     elif ones > 6:
-#         adjust = "+"
-#     else:
-#         adjust = ""
-#     return "DCB"[tens - 5] + adjust
-
 def ryerson_letter_grade(pct):
-    if 90 <= pct <= 100 or 100 <= pct <= 150:
-        return 'A+'
-    elif 85 <= pct <= 89:
-        return 'A'
-    elif 80 <= pct <= 84:
-        return 'A-'
-    elif 77 <= pct <= 79:
-        return 'B+'
-    elif 73 <= pct <= 76:
-        return 'B'
-    elif 70 <= pct <= 72:
-        return 'B-'
-    elif 67 <= pct <= 69:
-        return 'C+'
-    elif 63 <= pct <= 66:
-        return 'C'
-    elif 60 <= pct <= 62:
-        return 'C-'
-    elif 57 <= pct <= 59:
-        return 'D+'
-    elif 53 <= pct <= 56:
-        return 'D'
-    elif 50 <= pct <= 52:
-        return 'D-'
-    elif 0 <= pct <= 49:
-        return 'F'
+  if 90 <= pct <= 100 or 100 <= pct <= 150:
+      return 'A+'
+  elif 85 <= pct <= 89:
+      return 'A'
+  elif 80 <= pct <= 84:
+      return 'A-'
+  elif 77 <= pct <= 79:
+      return 'B+'
+  elif 73 <= pct <= 76:
+      return 'B'
+  elif 70 <= pct <= 72:
+      return 'B-'
+  elif 67 <= pct <= 69:
+      return 'C+'
+  elif 63 <= pct <= 66:
+      return 'C'
+  elif 60 <= pct <= 62:
+      return 'C-'
+  elif 57 <= pct <= 59:
+      return 'D+'
+  elif 53 <= pct <= 56:
+      return 'D'
+  elif 50 <= pct <= 52:
+      return 'D-'
+  elif 0 <= pct <= 49:
+      return 'F'
 
-# 16 seconds
-# def is_ascending(items):
-#     flag = 'True'
-#     if items == [] or len(items) == 1:
-#         return 'True'
-#     else:
-#         for count, value in enumerate(items):
-#             if count == 0:
-#                 continue
-#             elif value > items[count-1]:
-#                 continue
-#             elif value <= items[count-1]:
-#                 flag = 'False'
-#                 break
-#     return flag
-
-# 15.4 seconds
-# def is_ascending(items):
-#     # Flag any empty lists or lists with 1 item
-#     if items == [] or len(items) == 1:
-#         return 'True'
-#     # Flag any lists with duplicates
-#     elif len(items) != len(set(items)):
-#         return 'False'
-#     # Flag any lists that aren't ascendign
-#     else:
-#         flag = 'True'
-#         for count, value in enumerate(items):
-#             if count == 0:
-#                 continue
-#             elif items[count-1] - value > 0:
-#                 flag = 'False'
-#                 break
-#         return flag
 
 # 15.2 seconds
 def is_ascending(items):
-    # Flag any empty lists or lists with 1 item
-    if items == [] or len(items) == 1:
-        return True
-    # Flag any lists with duplicates
-    elif len(items) != len(set(items)):
-        return False
-    # Flag any lists that aren't ascendign
-    else:
-        flag = True
-        for count in range(len(items)-1):
-            if items[count+1] - items[count] < 0:
-                flag = False
-                break
-        return flag
+  return sorted(items) == items and len(set(items)) == len(items)
 
 def riffle(items, out=True):
-    if items == []:
-        return []
-    elif out == True:
-        rifl = []
-        for count in range(len(items)//2):
-            rifl.append(items[count])
-            rifl.append(items[count-len(items)//2])
-        return rifl
-    elif out == False:
-        rifl = []
-        for count in range(len(items)//2):
-            rifl.append(items[count-len(items)//2])
-            rifl.append(items[count])
-        return rifl
+  if out:
+      rifl = []
+      for count in range(len(items)//2):
+          rifl.append(items[count])
+          rifl.append(items[count-len(items)//2])
+      return rifl
+  else:
+      rifl = []
+      for count in range(len(items)//2):
+          rifl.append(items[count-len(items)//2])
+          rifl.append(items[count])
+      return rifl
 
 def only_odd_digits(n):
-    if n%2 == 0:
-        return False
-    else:
-        flag = True
-        for digit in str(n):
-            if int(digit)%2 == 0:
-                flag = False
-                break
-        return flag
+  if n%2 == 0:
+      return False
+  else:
+      for digit in str(n):
+          if int(digit)%2 == 0:
+              return False
+              break
+      return True
 
 def is_cyclops(n):
-    if n == 0:
-        return True
-    elif len(str(n))%2 == 0:
-        return False
-    elif str(n).count('0') > 1:
-        return False
-    elif str(n)[(len(str(n))-1)//2] == '0':
-        return True
-    else:
-        return False
+  if n == 0:
+      return True
+  elif len(str(n))%2 == 0:
+      return False
+  elif str(n).count('0') > 1:
+      return False
+  elif str(n)[(len(str(n))-1)//2] == '0':
+      return True
+  return False
